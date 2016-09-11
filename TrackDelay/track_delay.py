9b218c749144fba35e6bd71e124ed5553b558d8b
Ryan Batchelder <ryanbatch@gmail.com>
2016-09-11 13:40:56 -0400

    Adding error handling and retries when the network doesn't respond

--- TrackDelay/track_delay.py
+++ TrackDelay/track_delay.py
@@ -17,10 +17,19 @@ class TrackDelay(object):
 
     def process_delays(self):
         dal = data_access.DataAccess()
-        disabled_train_alerts = alert_scanner.AlertScanner.get_alerts()
-        for alert in disabled_train_alerts:
-            record = delay_dict.to_delay_dict(alert)
-            dal.add_delay_record(record)
+        retry_count = 3
+        while retry_count > 0:
+            disabled_train_alerts = alert_scanner.AlertScanner.get_alerts()
+            if disabled_train_alerts is not None:
+                retry_count = 0 # Stop retrying
+                for alert in disabled_train_alerts:
+                    record = delay_dict.to_delay_dict(alert)
+                    dal.add_delay_record(record)
+            else:
+                retry_count -= 1
+                logging.info("Retrying, disabled_train_alerts is empty")
+        if retry_count == 0:
+            logging.info("Error, retries failed. Sleeping until next run")
 
 td = TrackDelay()
 while True:
