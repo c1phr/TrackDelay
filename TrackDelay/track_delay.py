c6355245ac378dbfd9bc13ca555fe28bbc83004b
Ryan Batchelder <ryanbatch@gmail.com>
2016-09-21 19:38:00 -0400

    Adding handling for connection errors

--- TrackDelay/track_delay.py
+++ TrackDelay/track_delay.py
@@ -6,6 +6,7 @@ import data_access
 import logging
 import datetime
 import time
+from requests import exceptions
 
 
 class TrackDelay(object):
@@ -16,12 +17,18 @@ class TrackDelay(object):
         self.process_delays()
 
     def process_delays(self):
+        disabled_train_alerts = None
         dal = data_access.DataAccess()
         retry_count = 3
         while retry_count > 0:
-            disabled_train_alerts = alert_scanner.AlertScanner.get_alerts(None)
+            try:
+                disabled_train_alerts = alert_scanner.AlertScanner.get_alerts(None)
+            except exceptions.ConnectionError:
+                retry_count -= 1
+                logging.info("Retrying, connection error")
+
             if disabled_train_alerts is not None:
-                retry_count = 0 # Stop retrying
+                retry_count = 0  # Stop retrying
                 for alert in disabled_train_alerts:
                     record = delay_dict.to_delay_dict(alert)
                     dal.add_delay_record(record)
