bcabc7199e67233d06772e6c761971317aa80189
Ryan Batchelder <ryanbatch@gmail.com>
2016-10-28 20:34:44 -0400

    Increasing timeout for API calls

--- alert_scanner.py
+++ alert_scanner.py
@@ -13,7 +13,7 @@ class AlertScanner(object):
         url = "http://realtime.mbta.com/developer/api/v2/alerts?api_key={0}&include_access_alerts=false&format=json".format(
             api_key.get_key())
         try:
-            result = urllib2.urlopen(url)
+            result = urllib2.urlopen(url, timeout=60)
             full_data = result.read()
             full_data_json = json.loads(full_data)
             delay_data = [obj for obj in full_data_json["alerts"] if "cause_name" in obj]
